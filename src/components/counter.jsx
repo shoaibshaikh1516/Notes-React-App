23f0a8658659522da5e2e4bbfa08b00b4fa87c41
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-17 16:22:10 +0530

    setting Attribute

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
+    imageURL: 'https://picsum.photos/200',
   };
   render() {
     return (
       <React.Fragment>
+        <img src={this.state.imageURL} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment </button>
       </React.Fragment>

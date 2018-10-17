d67a8a5cc60e3c251e90eef5e765e630fce0446f
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-17 16:10:07 +0530

    formatCount fn

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
         <button>Increment </button>
-      </div>
+      </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 

e1bfcb6bfc166dc6380c375eefddeb6be99fbf5f
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-21 23:51:31 +0530

    toggle var value

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true,
+  };
+
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
+
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i className="fas fa-sort-down" />{' '}
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
         </h4>
         <ul className="list-group">
           <li className="list-group-item">

df16f627e14aad336ec957a67d172e93ab3d8f5c
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-21 21:44:42 +0530

    demo that construct is not require ,still it will work

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,25 +2,23 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 class Contacts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'Shyam',
-          email: 'shyam@gmail.com',
-          phone: '333-333-555',
-        },
-        {
-          id: 2,
-          name: 'Shoaib',
-          email: 'shoaib@gmail.com',
-          phone: '777-777-777',
-        },
-      ],
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Shyam',
+        email: 'shyam@gmail.com',
+        phone: '333-333-555',
+      },
+      {
+        id: 2,
+        name: 'Shoaib',
+        email: 'shoaib@gmail.com',
+        phone: '777-777-777',
+      },
+    ],
+  };
+
   render() {
     const { contacts } = this.state;
 

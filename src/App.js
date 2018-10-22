dc14020ffb4a11cf70aa472f858c470f69004b2c
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-22 16:14:10 +0530

    restructuring n AddContact added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Contacts from './components/Contacts';
-import Header from './components/Header';
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
 
 import { Provider } from './context';
 
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="App">
           <Header branding="CLNotes" />
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
         </div>
---
+++ src/components/contacts/AddContact.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Contact</h1>
+      </div>
+    );
+  }
+}
+
+export default AddContact;

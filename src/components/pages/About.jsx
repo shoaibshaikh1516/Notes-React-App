a2065d7a1b111c4fbd20f45803b1c6f6b629a6f5
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-23 16:58:23 +0530

    reverted back after testing

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,8 +6,6 @@ export default props => {
       <h1 className="display-4">About CLNotes</h1>
       <p className="lead">Simple App to manage</p>
       <p className="text-secondary">Version 1.0.0</p>
-
-      <h1 className="display-4">{props.match.params.id}</h1>
     </div>
   );
 };

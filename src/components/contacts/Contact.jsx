acc9bf1cd624f45e81f6c64cf5466ab10f6e6ba3
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-24 13:08:39 +0530

    https delete call to API

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
-
+import axios from 'axios';
 class Contact extends Component {
   state = {
     showContactInfo: false,
   };
 
   onDeleteClick = (id, dispatch) => {
-    dispatch({ type: 'DELETE_CONTACT', payload: id });
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(res => dispatch({ type: 'DELETE_CONTACT', payload: id }));
   };
 
   render() {

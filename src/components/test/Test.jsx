3ac49a3f2fd62137e497ae66d00bfad1f85e4c5c
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-10-24 12:48:29 +0530

    Get call using jsonplaceholder gel call

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   state = {
-    test: 'test',
+    body: '',
+    title: '',
   };
   componentDidMount() {
-    console.log('componentDidMount..');
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body,
+        })
+      );
   }
   // componentWillMount() {
   //   console.log('componentWillMount..');
@@ -28,9 +36,11 @@ class Test extends Component {
   // }
 
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }

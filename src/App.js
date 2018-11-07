94bfd6565198961b1d96b91f5614817efbc97a91
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-11-07 23:40:55 +0530

    cleared

--- src/App.js
+++ src/App.js
@@ -14,25 +14,22 @@ import AddNote from './components/notes/AddNote';
 import EditContact from './components/contacts/EditContact';
 import EditNote from './components/notes/EditNote';
 import Test from './components/test/Test';
-import SignUpBn from './components/pages/SignUpBn';
+// import SignUpBn from './components/pages/SignUpBn';
 import { Provider } from 'react-redux';
-import NewLogin from './components/login/NewLogin';
+// import NewLogin from './components/login/NewLogin';
 import store from './store';
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
           <div className="App">
-            {/* <Header branding="JOTter" /> */}
-
             <Header branding="JOTter / SEMIBREVE" />
             <div className="container">
               <Switch>
-                {/* <Route exact path="/" component={Contacts} /> //change */}
                 <Route exact path="/" component={Landing} /> //change
                 <Route exact path="/contact" component={Contacts} />
-                {/* <Route exact path="/login" component={Login} /> */}
                 {/* <Route exact path="/login" component={NewLogin} /> */}
                 <Route exact path="/login" component={Login} />
                 {/* <Route exact path="/signup" component={SignUpBn} /> */}

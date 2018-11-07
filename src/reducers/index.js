0f206f26bc61e0888bf3eb8f6b2787869a96c816
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-11-07 23:51:01 +0530

    auth Redux Reducer  default

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
 import contactReducer from './contactReducer';
 import noteReducer from './noteReducer';
+import authReducer from './authReducer';
 
-export default combineReducers({ contact: contactReducer, note: noteReducer });
+export default combineReducers({
+  contact: contactReducer,
+  note: noteReducer,
+  auth: authReducer,
+});
